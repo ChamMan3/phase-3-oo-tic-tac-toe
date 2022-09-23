f3db71399aa6e35f4e5bc6d36572bfc4abae6b04
LukeChamley <lkchamley@gmail.com>
2022-09-23 11:57:17 -0600

    commit tree 65f9da2ab6e9949103a18e9811ca5e8561e19b22

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,102 @@
+class TicTacToe
+    attr_accessor :board
+
+
+    def initialize
+        @board=[" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    end
+
+    WIN_COMBINATIONS = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6] 
+      ]
+    def display_board
+        puts row = [" #{board[0]} | #{board[1]} | #{board[2]} "]
+        puts seperator = "-----------"
+        puts row = [" #{board[3]} | #{board[4]} | #{board[5]} "]
+        puts seperator = "-----------"
+        puts row = [" #{board[6]} | #{board[7]} | #{board[8]} "]
+        puts seperator = "-----------"
+    end
+
+    def input_to_index(user_input)
+        user_input.to_i - 1
+    end
+
+    def move(index, players_token = x)
+        board[index] = players_token
+    end
+
+    def position_taken?(index)
+        if (board[index] == " ")
+            false
+        else
+            true
+        end
+    end
+
+    def valid_move?(index)
+        if !position_taken?(index) && index < 9
+            true
+        else
+            false
+        end
+    end
+
+    def turn_count
+        counter = 0
+        board.each do |spaces|
+            if spaces == "X" || spaces == "O"
+                counter += 1
+            end
+        end
+        counter
+    end
+
+    def current_player
+        turn_count.even? ? "X" : "O"
+    end
+
+    def turn 
+        user_input = gets.strip
+        index = input_to_index(user_input)
+        if valid_move?(index)
+            token = current_player
+             move(index, token)
+        else
+            turn
+        end
+        display_board
+    end
+
+    def won?
+
+    end
+
+    def full?
+
+    end
+
+    def draw?
+
+    end
+
+    def over?
+
+    end
+
+    def winner
+
+    end
+
+    def play 
+    
+    end
+
+end
